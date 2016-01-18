706b50953539009aa6404cb2416a65f97367cc0f
Benjin Yap <benjyap.dev@gmail.com>
2016-01-18 16:59:39 -0500

    init shunting yard

---
+++ TheCalculator/Models/ShuntingYard.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Diagnostics;
+namespace TheCalculator.Models {
+	public static class ShuntingYard {
+
+		public static void Main (string [] args) {
+			Debug.WriteLine (Shunt ("    1 +       1"));
+		}
+
+		public static string Shunt (string input) {
+			string output = "";
+
+			return output;
+		}
+	}
+}
