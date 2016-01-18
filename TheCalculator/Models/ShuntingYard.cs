e19dfbb8f5041b80eb50ba6786a03e14623432ff
Benjin Yap <benjyap.dev@gmail.com>
2016-01-18 17:37:58 -0500

    added token class and type

--- TheCalculator/Models/ShuntingYard.cs
+++ TheCalculator/Models/ShuntingYard.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System.Collections.Generic;
 using System.Diagnostics;
 namespace TheCalculator.Models {
 	public static class ShuntingYard {
@@ -9,9 +10,33 @@ namespace TheCalculator.Models {
 		}
 
 		public static string Shunt (string input) {
+			input = input.Replace (" ", "").ToLower ();
+
+			Stack <string> operatorStack = new Stack <string> ();
 			string output = "";
 
+			for (int i = 0; i < input.Length; i++) {
+				char c = input [i];
+
+
+			}
+			
 			return output;
+		}
+
+		private class Token {
+			public TokenType Type;
+			public string Value;
+
+			public Token (TokenType type, string value) {
+				this.Type = type;
+				this.Value = value;
+			}
+		}
+
+		private enum TokenType {
+			Number,
+			Operator,
 		}
 	}
 }
