cf98e3a0c0af307b1e476c86889d85ee512fe3a4
Benjin Yap <benjyap.dev@gmail.com>
2016-01-19 12:34:53 -0500

    fixed the operator shunting

--- TheCalculator/Models/ShuntingYard.cs
+++ TheCalculator/Models/ShuntingYard.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
@@ -7,16 +8,16 @@ namespace TheCalculator.Models {
 	public static class ShuntingYard {
 
 		public static void Main (string [] args) {
-			Debug.WriteLine (Shunt ("1+1"));
-			Debug.WriteLine (Shunt ("1*1"));
-			Debug.WriteLine (Shunt ("5-3"));
-			Debug.WriteLine (Shunt ("56/8"));
+			//Debug.WriteLine (Shunt ("1 * 5 - 2"));
+			Debug.WriteLine (Shunt ("1 + 5 - 3 * 5 / 7"));
+			//Debug.WriteLine (Shunt ("1 * 5 - 2"));
+			//Debug.WriteLine (Shunt ("1 * 5 - 2"));
 		}
 
 		public static string Shunt (string input) {
 			input = input.Replace (" ", "").ToLower ();
 
-			Stack <string> operatorStack = new Stack <string> ();
+			Stack <Operator> operatorStack = new Stack <Operator> ();
 			string output = "";
 			
 			//while input has stuff
@@ -38,15 +39,27 @@ namespace TheCalculator.Models {
 						output += token.Value + " ";
 						break;
 					case TokenType.Operator:
+						Operator op = Operator.Parse (token.Value);
+
+						//determine whether to pop stack operator into output
+						if (operatorStack.Count > 0) {
+							Operator topOperator = operatorStack.Peek ();
+							
+							if (op.Associativity == OperatorAssociativity.Left && op.Precedence <= topOperator.Precedence ||
+								op.Associativity == OperatorAssociativity.Right && op.Precedence < topOperator.Precedence) {
+								output += (char) operatorStack.Pop ().Type + " ";
+							}
+						}
+
 						//push operator to stack
-						operatorStack.Push (token.Value);
+						operatorStack.Push (op);
 						break;
 				}
 			}
 
 			//pop all remaining operators onto stack
 			while (operatorStack.Count > 0) {
-				output += operatorStack.Pop () + " ";
+				output += (char) operatorStack.Pop ().Type + " ";
 			}
 			
 			//trim the trailing space and return output
@@ -56,8 +69,8 @@ namespace TheCalculator.Models {
 		private static Token GetToken (string input) {
 			//create the token type regex patterns
 			Dictionary <TokenType, string> tokenPatterns = new Dictionary <TokenType, string> ();
-			tokenPatterns [TokenType.Number] = @"\d+";
-			tokenPatterns [TokenType.Operator] = @"[+\-*/]";
+			tokenPatterns [TokenType.Number] = @"^\d+";
+			tokenPatterns [TokenType.Operator] = @"^[+\-*/]";
 			
 			//check input against each pattern
 			foreach (KeyValuePair <TokenType, string> pair in tokenPatterns) {
@@ -96,6 +109,51 @@ namespace TheCalculator.Models {
 		private enum TokenType {
 			Number,
 			Operator,
+		}
+
+		private class Operator {
+			public OperatorType Type;
+			public int Precedence;
+			public OperatorAssociativity Associativity;
+
+			public static Operator Parse (string value) {
+				Operator op = new Operator ();
+
+				//set the operator type based on the value
+				op.Type = (OperatorType) Enum.ToObject (typeof (OperatorType), (int) value [0]);
+
+				//set the operator precedence based on the value
+				string [] precedences = { "+-", "*/", "^" };
+
+				for (int i = 0; i < precedences.Length; i++) {
+					if (precedences [i].Contains (value)) {
+						op.Precedence = i;
+						break;
+					}
+				}
+
+				//set the operator associativity based on the value
+				if (value == "^") {
+					op.Associativity = OperatorAssociativity.Right;
+				} else {
+					op.Associativity = OperatorAssociativity.Left;
+				}
+
+				return op;
+			}
+		}
+
+		private enum OperatorType {
+			Add = '+',
+			Subtract = '-',
+			Multiply = '*',
+			Divide = '/',
+			Exponent = '^',
+		}
+
+		private enum OperatorAssociativity {
+			Left,
+			Right
 		}
 	}
 }
