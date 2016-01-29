48734ecc08f95805d03d8beeab7d10b4023456f1
Benjin Yap <benjyap.dev@gmail.com>
2016-01-28 20:15:39 -0500

    init

---
+++ TheCalculator/Models/Calcumalator.cs
@@ -0,0 +1,10 @@
+﻿
+
+using System.Diagnostics;
+namespace TheCalculator.Models {
+	public static class Calcumalator {
+		public static void Main (string [] args) {
+			Debug.WriteLine (123);
+		}
+	}
+}
