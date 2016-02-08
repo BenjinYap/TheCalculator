dd12b4529ca5560b16b7d59461f1874722e8de0a
Benjin Yap <benjyap.dev@gmail.com>
2016-02-07 23:08:35 -0500

    error label hides when no error

---
+++ TheCalculator/Views/Converters.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+namespace TheCalculator.Views {
+
+	//public class IndexToBackgroundConverter:IValueConverter {
+		
+	//	public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
+	//		return Brushes.Red;
+	//	}
+
+	//	public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
+	//		throw new NotImplementedException ();
+	//	}
+	//}
+
+	public class LengthToVisibilityConverter:IValueConverter {
+		public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
+			return string.IsNullOrEmpty (value as string) ? Visibility.Collapsed : Visibility.Visible;
+		}
+
+		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
+			throw new NotImplementedException ();
+		}
+	}
+}
