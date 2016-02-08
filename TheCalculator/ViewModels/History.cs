07fbc17b8233ab707c6d895206dec89d324c29fe
Benjin Yap <benjyap.dev@gmail.com>
2016-02-07 19:50:12 -0500

    view models

---
+++ TheCalculator/ViewModels/History.cs
@@ -0,0 +1,8 @@
+﻿
+
+using System.Collections.ObjectModel;
+namespace TheCalculator.ViewModels {
+	public class History:ObservableCollection <HistoryItem> {
+
+	}
+}
---
+++ TheCalculator/ViewModels/HistoryItem.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace TheCalculator.ViewModels {
+	public class HistoryItem {
+		public string Input { get; set; }
+		public string Output { get; set; }
+	}
+}
