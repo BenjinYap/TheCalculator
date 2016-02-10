63e15cff3e189b0bfc8e77db921578560d519e4d
Benjin Yap <benjyap.dev@gmail.com>
2016-02-10 17:09:55 -0500

    moved tests to own class

--- TheCalculator/Models/Calcumalator.cs
+++ TheCalculator/Models/Calcumalator.cs
@@ -5,68 +5,7 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 namespace TheCalculator.Models {
 	public static class Calcumalator {
-		public static void Main (string [] args) {
-			BadAssert ("(1+1*2+sin(0)", CalcumalateError.MissingCloseBracket);
-			BadAssert ("1+1*1/(1+1^(sin(0))))", CalcumalateError.MissingOpenBracket);
-			BadAssert ("1&1", CalcumalateError.UnknownOperator);
-			BadAssert ("1_1", CalcumalateError.UnknownOperator);
-			BadAssert ("1++1", CalcumalateError.SyntaxError);
-			BadAssert ("1**1", CalcumalateError.SyntaxError);
-			BadAssert ("*/1--1", CalcumalateError.SyntaxError);
-			BadAssert ("_!41--1", CalcumalateError.UnknownOperator);
-			BadAssert ("++++++++", CalcumalateError.SyntaxError);
-			BadAssert ("----", CalcumalateError.SyntaxError);
-			BadAssert (")))(((", CalcumalateError.SyntaxError);
-			//BadAssert ("8-^738^2^^", CalcumalateError.SyntaxError);
-			//BadAssert ("9^4/*035*1", CalcumalateError.SyntaxError);
-			//BadAssert ("3^5462/07+", CalcumalateError.SyntaxError);
-			//BadAssert ("+636/9927-", CalcumalateError.SyntaxError);
-			//BadAssert ("-16/5-4757", CalcumalateError.SyntaxError);
-			//BadAssert ("4148+4-9-6", CalcumalateError.SyntaxError);
-			//BadAssert ("+5+0620-00", CalcumalateError.SyntaxError);
-			//BadAssert ("2/3912-15*", CalcumalateError.SyntaxError);
-			//BadAssert ("0367315^5/", CalcumalateError.SyntaxError);
-			//BadAssert ("1847/1-24+", CalcumalateError.SyntaxError);
-
-			Assert ("-1+1", 0);
-			Assert ("1--1", 2);
-			Assert ("sin(sin(0))", 0);
-			Assert ("(((sin((((0)))))))", 0);
-			Assert ("(-1)", -1);
-			Assert ("-1", -1);
-			Assert ("1", 1);
-			Assert ("1*2+2", 4);
-			Assert ("1+1*2-1*2", 1);
-			Assert ("1*2^2^2", 16);
-			Assert ("1*2+1+4/2", 5);
-			Assert ("1", 1);
-			Assert ("1+1-1", 1);
-			Assert ("1-1+1", 1);
-			Assert ("1+1*3", 4);
-			Assert ("4/2", 2);
-			Assert ("2^2", 4);
-			Assert ("(1+1)*2", 4);
-			
-			Assert ("1+-1", 0);
-			Assert ("1+-1+1", 1);
-
-			Assert ("sin(0)", 0);
-			Assert ("tan(0)", 0);
-			Assert ("cos(0)", 1);
-			Assert ("tan(0)", 0);
-			Assert ("sinh(0)", 0);
-			Assert ("cosh(0)", 1);
-			Assert ("tanh(0)", 0);
-			Assert ("asin(0)", 0);
-			Assert ("acos(1)", 0);
-			Assert ("atan(0)", 0);
-			Assert ("abs(-1)", 1);
-			Assert ("abs(1)", 1);
-			Assert ("abs(-1+abs(-1))", 0);
-
-			//Assert ("pi", Math.PI);
-		}
-
+		
 		private static List <string> operators;
 		private static List <string> functions;
 		private static List <string> constants;
@@ -75,24 +14,6 @@ namespace TheCalculator.Models {
 			operators = new List <string> { "+", "-", "*", "/", "^" };
 			functions = new List <string> { "asin", "acos", "atan", "sinh", "cosh", "tanh", "sin", "cos", "tan", "abs", "neg" };
 			constants = new List <string> { "π", "pi" };
-		}
-
-		public static void BadAssert (string input, CalcumalateError output) {
-			CalcumalateResult result = Calcumalate (input);
-
-			if (result.Error != output) {
-				Debug.WriteLine (input + " = " + output.ToString () + ", GOT " + result.Error.ToString ());
-			}
-		}
-
-		public static void Assert (string input, double output) {
-			CalcumalateResult result = Calcumalate (input);
-
-			if (result.Result == output) {
-				
-			} else {
-				Debug.WriteLine (input + " = " + output + ", GOT " + result.Result);
-			}
 		}
 
 		public static CalcumalateResult Calcumalate (string input) {
---
+++ TheCalculator/UnitTest.cs
@@ -0,0 +1,88 @@
+﻿
+
+using System.Diagnostics;
+using TheCalculator.Models;
+namespace TheCalculator {
+	public class UnitTest {
+		public static void Main (string [] args) {
+			BadAssert ("(1+1*2+sin(0)", CalcumalateError.MissingCloseBracket);
+			BadAssert ("1+1*1/(1+1^(sin(0))))", CalcumalateError.MissingOpenBracket);
+			BadAssert ("1&1", CalcumalateError.UnknownOperator);
+			BadAssert ("1_1", CalcumalateError.UnknownOperator);
+			BadAssert ("1++1", CalcumalateError.SyntaxError);
+			BadAssert ("1**1", CalcumalateError.SyntaxError);
+			BadAssert ("*/1--1", CalcumalateError.SyntaxError);
+			BadAssert ("_!41--1", CalcumalateError.UnknownOperator);
+			BadAssert ("++++++++", CalcumalateError.SyntaxError);
+			BadAssert ("----", CalcumalateError.SyntaxError);
+			BadAssert (")))(((", CalcumalateError.SyntaxError);
+			//BadAssert ("8-^738^2^^", CalcumalateError.SyntaxError);
+			//BadAssert ("9^4/*035*1", CalcumalateError.SyntaxError);
+			//BadAssert ("3^5462/07+", CalcumalateError.SyntaxError);
+			//BadAssert ("+636/9927-", CalcumalateError.SyntaxError);
+			//BadAssert ("-16/5-4757", CalcumalateError.SyntaxError);
+			//BadAssert ("4148+4-9-6", CalcumalateError.SyntaxError);
+			//BadAssert ("+5+0620-00", CalcumalateError.SyntaxError);
+			//BadAssert ("2/3912-15*", CalcumalateError.SyntaxError);
+			//BadAssert ("0367315^5/", CalcumalateError.SyntaxError);
+			//BadAssert ("1847/1-24+", CalcumalateError.SyntaxError);
+
+			Assert ("-1+1", 0);
+			Assert ("1--1", 2);
+			Assert ("sin(sin(0))", 0);
+			Assert ("(((sin((((0)))))))", 0);
+			Assert ("(-1)", -1);
+			Assert ("-1", -1);
+			Assert ("1", 1);
+			Assert ("1*2+2", 4);
+			Assert ("1+1*2-1*2", 1);
+			Assert ("1*2^2^2", 16);
+			Assert ("1*2+1+4/2", 5);
+			Assert ("1", 1);
+			Assert ("1+1-1", 1);
+			Assert ("1-1+1", 1);
+			Assert ("1+1*3", 4);
+			Assert ("4/2", 2);
+			Assert ("2^2", 4);
+			Assert ("(1+1)*2", 4);
+			
+			Assert ("1+-1", 0);
+			Assert ("1+-1+1", 1);
+
+			Assert ("sin(0)", 0);
+			Assert ("tan(0)", 0);
+			Assert ("cos(0)", 1);
+			Assert ("tan(0)", 0);
+			Assert ("sinh(0)", 0);
+			Assert ("cosh(0)", 1);
+			Assert ("tanh(0)", 0);
+			Assert ("asin(0)", 0);
+			Assert ("acos(1)", 0);
+			Assert ("atan(0)", 0);
+			Assert ("abs(-1)", 1);
+			Assert ("abs(1)", 1);
+			Assert ("abs(-1+abs(-1))", 0);
+
+			//Assert ("pi", Math.PI);
+		}
+
+		public static void BadAssert (string input, CalcumalateError output) {
+			CalcumalateResult result = Calcumalator.Calcumalate (input);
+
+			if (result.Error != output) {
+				Debug.WriteLine (input + " = " + output.ToString () + ", GOT " + result.Error.ToString ());
+			}
+		}
+
+		public static void Assert (string input, double output) {
+			CalcumalateResult result = Calcumalator.Calcumalate (input);
+
+			if (result.Result == output) {
+				
+			} else {
+				Debug.WriteLine (input + " = " + output + ", GOT " + result.Result);
+			}
+		}
+
+	}
+}
